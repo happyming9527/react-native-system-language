92cd70f9a511f4842533a3bb3365481533910261
happyming9527 <happyming9527@gmail.com>
2016-08-14 10:43:41 +0800

    添加android获取系统语言的功能

---
+++ android/src/main/java/com/publicbuy/systemlanguage/RNSystemLanguageModule.java
@@ -0,0 +1,42 @@
+
+package com.publicbuy.systemlanguage;
+
+import android.text.TextUtils;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import java.lang.RuntimeException;
+import java.util.Locale;
+
+public class RNSystemLanguageModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+  private static final String ErrorMessage = "current system language fetch failed.";
+  public RNSystemLanguageModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNSystemLanguage";
+  }
+
+  @ReactMethod
+  public void getLanguageName(Promise promise) {
+    try {
+//      String name = Locale.getDefault().getLanguage();
+      String name = "";
+      if (TextUtils.isEmpty(name)) {
+        throw new RuntimeException(ErrorMessage);
+      }
+      promise.resolve(name);
+    } catch (RuntimeException e) {
+      promise.reject(e.getMessage());
+    }
+  }
+}
--- android/src/main/java/com/publicbuy/systemlanguage/RNSystemLanguagePackage.java
+++ android/src/main/java/com/publicbuy/systemlanguage/RNSystemLanguagePackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.publicbuy.systemlanguage;
 
 import java.util.Arrays;
 import java.util.Collections;
